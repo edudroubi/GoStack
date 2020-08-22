9632b8c99828c09e7bef498bc4e0269d7ea9eff3
edudroubi <eduardodroubi@gmail.com>
2020-08-22 12:47:12 -0300

    final version of frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+import './App.css';
 import Header from './components/Header';
 
 function App () {
+  
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    })
+  }, []);
+
+  async function handleAddProject () {
+    // Não aplica o conceito de imutabilidade
+    // projects.push(`Novo Projeto ${Date.now()}`);
+    
+    // Aplica o conceito de imutabilidade
+    // setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+    
+    const response = await api.post('projects', {
+      title: `Novo Projeto ${Date.now()}`,
+      owner: "Eduardo Droubi"
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
-      <Header title='Homepage'>
-        <ul>
-          <li>Name</li>
-          <li>Age</li>
-        </ul>
-      </Header>
-      <Header title='Projects'>
-        <ul>
-          <li>Project 1</li>
-          <li>Project 2</li>
-        </ul>
-      </Header>
+      <Header title='Projects' />
+
+      <ul>
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Add Project</button>
     </>
   );
 }
