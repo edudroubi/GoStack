02ae8e152fbbe80a5b19d510732203cefbbde98d
edudroubi <eduardodroubi@gmail.com>
2020-08-25 10:39:27 -0300

    add first node project with typescritp files

---
+++ first-node-project/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (resquest, response) => {
+  return response.json({ message: "Hello World" });
+});
+
+app.listen(3333, () => {
+  console.log('Server started at http://localhost:3333');
+});
