8d926235a72c237ac47e07a5a371008c06f8f990
edudroubi <eduardodroubi@gmail.com>
2020-08-30 10:54:57 -0300

    Create users DB, create user service, create user route and create user models

--- first-node-project/src/routes/index.ts
+++ first-node-project/src/routes/index.ts
@@ -1,9 +1,11 @@
 // src/routes/index.ts
 import { Router } from 'express';
 import appoitmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appoitments', appoitmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
